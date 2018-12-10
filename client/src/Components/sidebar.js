51809c532bbc57197bf21cc1680b9fcea28b6919
Albin Groen <albin.groen@gmail.com>
2018-12-10 19:48:04 +0100

    Small fixes

--- client/src/Components/sidebar.js
+++ client/src/Components/sidebar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const SidebarWrapper = styled.div`
